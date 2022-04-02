2077b28875c8034e53f3e857deb55c6a6e07a650
Alberto Xamin <alberto.xamin@datadoghq.com>
2022-04-02 11:26:11 +0200

    change formatting

--- src/common.ts
+++ src/common.ts
@@ -1,32 +1,33 @@
 export function resToDetail(word: string, res: any) {
-  let detail = "# " + word + "\n\n";
+  let detail = "## " + word + "\n\n";
   try {
     if (!res.translations.empty) {
       for (const x of res.translations.entryGroups) {
         detail += `##### ${x.gategory}\n---\n`;
         for (const y of x.entries) {
-          detail += `${y.text} ${y.context ? "_" + y.context + "_" : ""}\n\n`;
+          detail += `1. ${y.text.replace("[[", "(").replace("]]", ")")} ${y.context ? "_" + y.context + "_" : ""}\n\n`;
         }
       }
-    } else if (!res.definitions.empty) {
+    }
+    if (!res.definitions.empty) {
       for (const x of res.definitions.entryGroups) {
         detail += `##### ${x.gategory}\n---\n`;
         for (const y of x.entries) {
           detail += `${y.text}\n\n`;
         }
       }
-    } else if (res.definitionsInDestLanguage && !res.definitionsInDestLanguage.empty) {
+    }
+    if (res.definitionsInDestLanguage && !res.definitionsInDestLanguage.empty) {
+      detail += `##### DEFINITIONS IN DEST LANGUAGE\n---\n`;
+      const regex = /\[\[([a-z]+)#(Finnish|English)\|\1\]\]/gm;
       for (const x of res.definitionsInDestLanguage.entryGroups) {
-        detail += `##### ${x.gategory}\n---\n`;
+        detail += `* ${x.gategory}\n`;
         for (const y of x.entries) {
-          detail += `${y.text}\n\n`;
+          detail += `\t1. ${y.text.replace(regex, '$1')}\n`;
         }
+        detail += `\n`;
       }
-    } else {
-      detail += `##### NOT FOUND\n---\n`;
-      for (const x in res.suggestions) {
-        detail += `${x}\n\n`;
-      }
+      
     }
     if (res.subtitleResult && res.subtitleResult.resultList) {
       detail += `##### EXAMPLES\n---\n`;
